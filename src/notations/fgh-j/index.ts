beef4038ba27da97cc0884fa287ae1937e3a0cb7
VeryrrDefine <1308061599@qq.com>
2025-09-08 18:36:45 +0800

    add precheck

--- src/notations/fgh-j/index.ts
+++ src/notations/fgh-j/index.ts
@@ -1,14 +1,30 @@
 import PowiainaNum from "powiaina_num.js";
 import { Notation } from "../notation-class";
 import formatXEY from "./xey-format";
-
+export function preCheckString(
+  num: PowiainaNum,
+  t: {
+    NaNString: string;
+    PosInfString: string;
+    NegInfString: string;
+  }
+) {
+  if (num.isNaN()) return t.NaNString;
+  if (num.isInfi()) {
+    return num.sign == 1 ? t.PosInfString : t.NegInfString;
+  }
+}
 export class FGHJNotation extends Notation {
   format(num: PowiainaNum, precision?: number): string {
+    const precheck = preCheckString(num, this);
+    if (precheck) return precheck;
     return formatXEY(num, precision);
   }
 }
 export class EngineeringNotation extends Notation {
   format(num: PowiainaNum, precision?: number): string {
+    const precheck = preCheckString(num, this);
+    if (precheck) return precheck;
     return formatXEY(num, precision, {
       engineering: true,
       UCF: false,
